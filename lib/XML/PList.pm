a118a188c6c90fbc7cc22a7e1ee12a6d8cea3579
Sergey Mudrik <sergey.mudrik@gmail.com>
2012-02-26 16:29:58 +0400

    first empty version

---
+++ lib/XML/PList.pm
@@ -0,0 +1,54 @@
+package XML::PList;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+sub new {
+
+}
+
+sub parse {
+
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+XML::PList - parse mac .plist files
+
+=head1 SYNOPSIS
+
+  use XML::PList;
+  my $data = XML::PList->new('file.plist')->parse();
+
+=head1 DESCRIPTION
+
+Read mac plist files (xml for now)
+
+=head2 EXPORT
+
+None by default.
+
+=head1 SEE ALSO
+
+L<Mac::Tie::PList>,
+L<Data::Plist>
+
+=head1 AUTHOR
+
+Sergey Mudrik, E<lt>sergey.mudrik@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Sergey Mudrik
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
